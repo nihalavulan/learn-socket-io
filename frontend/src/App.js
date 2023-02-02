295e9f40a686cfe2211f046115d97ec648de33cb
Nihal Avulan <nihalavulan1@gmail.com>
2023-02-02 23:53:05 +0530

    connected frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Route } from 'react-router-dom';
 import './App.css';
+import ChatPage from './Pages/ChatPage';
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Route path={'/'}/>
+      <Route path={'/'} component={HomePage} exact/>
+      <Route path={'/chats'} component={ChatPage}/>
     </div>
   );
 }
