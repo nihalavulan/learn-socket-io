704d221bb3d89e62e9660fedd0f7182e6d21f39c
Nihal Avulan <nihalavulan1@gmail.com>
2023-02-02 23:44:06 +0530

    frontend basics

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Route path={'/'}/>
+    </div>
+  );
+}
+
+export default App;
